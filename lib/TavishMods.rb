c077316cb5127d6983fabc9e2f3b6fd4f471595c
Tavish Armstrong <tavish@evolvingweb.ca>
2011-09-07 10:11:01 -0400

    Error handling on timelog submit

--- lib/TavishMods.rb
+++ lib/TavishMods.rb
@@ -9,13 +9,18 @@ module TavishMods
       if context[:params][:action] == 'create'
         # get the user id submitted by the form
         user_id = context[:params][:user][:user_id]
-        selected_user = User.find(user_id.to_i)
+        begin
+          selected_user = User.find(user_id.to_i)
 
-        # get the time_entry given by the hook
-        time_entry = context[:time_entry]
+          # get the time_entry given by the hook
+          time_entry = context[:time_entry]
 
-        # set the user
-        time_entry.attributes = { :user => selected_user }
+          # set the user
+          time_entry.attributes = { :user => selected_user }
+        rescue
+          # don't do anything. The hook caller will have set the user
+          # with User.current
+        end
       end
     end
   end
